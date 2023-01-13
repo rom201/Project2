bda8afac2f89c1273ec86a383d9b95c9926750c3
rom201 <kovalchukr1104@gmail.com>
2023-01-12 22:48:26 -0500

    second commit

---
+++ src/Day1/Test1.java
@@ -0,0 +1,7 @@
+package Day1;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/HelloThere.java
+++ src/HelloThere.java
@@ -1,5 +1,6 @@
 public class HelloThere {
     public static void main(String[] args) {
         System.out.println("Hi I am in");
+
     }
 }
